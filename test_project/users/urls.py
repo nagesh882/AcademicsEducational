33b16e9e8e18a2bc43d5a179c7b324e971357a8b
Nagesh <yashchavan192001@gmail.com>
2023-12-13 21:35:12 +0530

    some kind of pages has been improved with the help of django

--- test_project/users/urls.py
+++ test_project/users/urls.py
@@ -10,6 +10,24 @@ urlpatterns = [
 
     path('contact', views.contact, name='contact'),
 
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+
+    path('login', views.login, name='login'),
+
+    path('register', views.register, name='register'),
+
+    path('python', views.python, name='python'),
+
+    path('news-single', views.news_single, name='news-single'),
+
+    path('experence', views.experence, name='experence'),
+
+    path('fplace', views.fplace, name='fplace'),
+
+    path('eplace', views.eplace, name='eplace'),
+
+    path('tens', views.tens, name='tens'),
+
+    path('campus', views.campus, name='campus'),
 
 ]
--- test_project/users/views.py
+++ test_project/users/views.py
@@ -39,8 +39,113 @@ def contact(request):
     return HttpResponse(template.render(context))
 
 
+def login(request):
+    template = loader.get_template("login.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+
 def about(request):
     template = loader.get_template("about.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+
+def register(request):
+    template = loader.get_template("register.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+
+def python(request):
+    template = loader.get_template("python.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+def news_single(request):
+    template = loader.get_template("news-single.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+
+def experence(request):
+    template = loader.get_template("experence.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+
+def fplace(request):
+    template = loader.get_template("fplace.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+
+def eplace(request):
+    template = loader.get_template("eplace.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+def tens(request):
+    template = loader.get_template("tens.html")
+    
+    user_list = User.objects.all().values()
+
+    context = {
+        "user_list":user_list
+    }
+
+    return HttpResponse(template.render(context))
+
+def campus(request):
+    template = loader.get_template("campus.html")
     
     user_list = User.objects.all().values()
 
